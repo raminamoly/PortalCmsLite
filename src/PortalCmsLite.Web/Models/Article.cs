fc77a6843f8a48bea533a7109686e1c63e3e22d9
Ramin Amoly <37180501+raminamoly@users.noreply.github.com>
2026-06-01 13:11:08 +0330

    Add article model

---
+++ src/PortalCmsLite.Web/Models/Article.cs
@@ -0,0 +1,8 @@
+namespace PortalCmsLite.Web.Models;
+
+public class Article
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+}
