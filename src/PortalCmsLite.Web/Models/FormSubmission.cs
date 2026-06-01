1249f620ac3778c3a42a8ef5abc719fc83f77566
Ramin Amoly <37180501+raminamoly@users.noreply.github.com>
2026-06-01 13:42:29 +0330

    Add form submission model

---
+++ src/PortalCmsLite.Web/Models/FormSubmission.cs
@@ -0,0 +1,9 @@
+namespace PortalCmsLite.Web.Models;
+
+public class FormSubmission
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
